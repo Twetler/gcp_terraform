f727cd65d3189035827bac89c833e14028a4106a
Tulio <tuliowetler@gmail.com>
2025-02-05 22:52:17 +0000

    Final touches

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "6.8.0"
     }
   }
@@ -11,8 +11,3 @@ provider "google" {
   project = "cloud-eng-1"
   region  = "europe-west1"
 }
-
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
-}
-
